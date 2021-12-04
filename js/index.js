e1ad0b823601ec250317a39139b808dc45b3b067
Farruh Olimov <farruhbekolimov3904@gmail.com>
2021-12-04 21:56:32 +0500

    add light and plane

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
-let scene, camera, renderer, cube, line
+let scene, camera, renderer, loader, cube, line, light, floor
+
+
+window.addEventListener("resize", onWindowResize, false)
+
+init()
+animate()
+
+
 function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     renderer = new THREE.WebGLRenderer({ antialias: true });
-    const loader = new THREE.GLTFLoader()
+    loader = new THREE.GLTFLoader()
+
+    scene.background = new THREE.Color("#bbbbbb");
+    scene.fog = new THREE.Fog( "#fff", 20, 100 );
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.position.set(0, 0, 3)
@@ -16,29 +27,38 @@ function init() {
     points.push(new THREE.Vector3(0, 1, 0))
     points.push(new THREE.Vector3(-2, 0, 0))
 
-    const light = new THREE.AmbientLight("#fff");
+    light = new THREE.PointLight( 0xffee88, 4, 100, 2 );
     const floor_geometry = new THREE.PlaneGeometry(20, 20);
     const cube_geometry = new THREE.BoxGeometry();
     const line_geometry = new THREE.BufferGeometry().setFromPoints(points);
 
     const crate_texture = new THREE.TextureLoader().load('../textures/crate.jpg') 
-    const grid_texture = new THREE.TextureLoader().load('../textures/grid.jpg') 
+    const grid_texture = new THREE.TextureLoader().load('../textures/grid.png') 
 
     const cube_material = new THREE.MeshBasicMaterial({
         map: crate_texture
     });
+    const floor_material = new THREE.MeshBasicMaterial({
+        map: grid_texture
+    })
     const line_material = new THREE.LineBasicMaterial({
         color: "#ffffff"
     });
 
 
+    floor = new THREE.Mesh(floor_geometry, floor_material)
     cube = new THREE.Mesh(cube_geometry, cube_material)
     line = new THREE.Line(line_geometry, line_material)
+
+    floor.receiveShadow = true
+    floor.rotation.x = (-Math.PI / 2.0);
+    floor.position.set(0, -2, 0)
 
     cube.rotation.set(0.8, 2.35, 0)
 
     // scene.add(cube)
     scene.add(light)
+    scene.add(floor)
     scene.add(line)
 
     loader.load("../models/RoboAnt.gltf", function(gltf){
@@ -53,7 +73,7 @@ function init() {
     } )
 }
 
-const animate = function () {
+function animate () {
     requestAnimationFrame(animate);
 
     cube.rotation.y += 0.01
@@ -66,8 +86,3 @@ function onWindowResize(){
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
-
-window.addEventListener("resize", onWindowResize, false)
-
-init()
-animate()
