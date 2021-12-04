16664a8f85f2dba8d9c39da0a8f1306808e7c121
Farruh Olimov <farruhbekolimov3904@gmail.com>
2021-12-04 17:54:35 +0500

    draw line

--- js/index.js
+++ js/index.js
@@ -3,16 +3,27 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.set(0, 0, 3)
+camera.lookAt(0, 0, 0)
 
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00  });
+const points = []
+points.push(new THREE.Vector3(2, 0, 0))
+points.push(new THREE.Vector3(0, 1, 0))
+points.push(new THREE.Vector3(-2, 0, 0))
 
-const cube  = new THREE.Mesh(geometry, material)
+const cube_geometry = new THREE.BoxGeometry();
+const line_geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const cube_material = new THREE.MeshBasicMaterial({ color: "#364f63"  });
+const line_material = new THREE.LineBasicMaterial({ color: "#ffffff"  });
+
+const cube  = new THREE.Mesh(cube_geometry, cube_material)
+const line  = new THREE.Line(line_geometry, line_material)
+
 scene.add(cube)
-
-camera.position.z = 5
+scene.add(line)
 
 const animate = function(){
     requestAnimationFrame(animate);
