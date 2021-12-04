47a9dcffb03404999763f9cf2e179f90f150a325
Farruh Olimov <farruhbekolimov3904@gmail.com>
2021-12-04 19:11:46 +0500

    import own 3d model via gltf loader

--- js/index.js
+++ js/index.js
@@ -1,37 +1,73 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+let scene, camera, renderer, cube, line
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    const loader = new THREE.GLTFLoader()
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.set(0, 0, 3)
-camera.lookAt(0, 0, 0)
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.set(0, 0, 3)
+    camera.lookAt(0, 0, 0)
 
-document.body.appendChild(renderer.domElement)
+    document.body.appendChild(renderer.domElement)
 
-const points = []
-points.push(new THREE.Vector3(2, 0, 0))
-points.push(new THREE.Vector3(0, 1, 0))
-points.push(new THREE.Vector3(-2, 0, 0))
+    const points = []
+    points.push(new THREE.Vector3(2, 0, 0))
+    points.push(new THREE.Vector3(0, 1, 0))
+    points.push(new THREE.Vector3(-2, 0, 0))
 
-const cube_geometry = new THREE.BoxGeometry();
-const line_geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const light = new THREE.AmbientLight("#fff");
+    const floor_geometry = new THREE.PlaneGeometry(20, 20);
+    const cube_geometry = new THREE.BoxGeometry();
+    const line_geometry = new THREE.BufferGeometry().setFromPoints(points);
 
-const cube_material = new THREE.MeshBasicMaterial({ color: "#364f63"  });
-const line_material = new THREE.LineBasicMaterial({ color: "#ffffff"  });
+    const crate_texture = new THREE.TextureLoader().load('../textures/crate.jpg') 
+    const grid_texture = new THREE.TextureLoader().load('../textures/grid.jpg') 
 
-const cube  = new THREE.Mesh(cube_geometry, cube_material)
-const line  = new THREE.Line(line_geometry, line_material)
+    const cube_material = new THREE.MeshBasicMaterial({
+        map: crate_texture
+    });
+    const line_material = new THREE.LineBasicMaterial({
+        color: "#ffffff"
+    });
 
-scene.add(cube)
-scene.add(line)
 
-const animate = function(){
+    cube = new THREE.Mesh(cube_geometry, cube_material)
+    line = new THREE.Line(line_geometry, line_material)
+
+    cube.rotation.set(0.8, 2.35, 0)
+
+    // scene.add(cube)
+    scene.add(light)
+    scene.add(line)
+
+    loader.load("../models/RoboAnt.gltf", function(gltf){
+        let obj = gltf.scene
+        obj.position.set(0, 0, -7)
+        obj.rotation.set(0, 11, 0)
+        scene.add(obj)
+    }, undefined, function ( error ) {
+
+        console.error( error );
+    
+    } )
+}
+
+const animate = function () {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01
     cube.rotation.y += 0.01
 
     renderer.render(scene, camera)
 }
 
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", onWindowResize, false)
+
+init()
 animate()
